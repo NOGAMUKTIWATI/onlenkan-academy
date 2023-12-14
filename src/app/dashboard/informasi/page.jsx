e64cd58a0489bbeae2bda92a5fe9132d71e36812
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-12-14 23:54:29 +0700

    navbar informasi

---
+++ src/app/dashboard/informasi/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="informasi">
+    
+      <h2 className="font-bold pl-3 text-start text-2xl py-5">
+       Informasi Terkini
+      </h2>
+      
+    </div>
+  );
+}
