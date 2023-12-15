8f703a98c9c69177f5f9fc882492f4946f36a0df
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-12-15 09:41:38 +0700

    card informasi

--- src/app/dashboard/informasi/page.jsx
+++ src/app/dashboard/informasi/page.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Cardinfo from "./cardinfo";
+
 
 export default function page() {
   return (
-    <div className="informasi">
-    
-      <h2 className="font-bold pl-3 text-start text-2xl py-5">
-       Informasi Terkini
-      </h2>
-      
+    <div className="p-5 text-center">
+      <div className="informasi">
+        <h2 className="font-bold pl-3 text-start text-2xl py-5">
+          Informasi Terkini
+        </h2>
+        <Cardinfo/>
+      </div>
     </div>
   );
 }
