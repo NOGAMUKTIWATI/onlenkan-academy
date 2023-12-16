ead5f536251779a00b409438cc1d5a74adad80f5
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-12-16 14:39:12 +0700

    page

--- src/app/dashboard/informasi/page.jsx
+++ src/app/dashboard/informasi/page.jsx
@@ -8,20 +8,20 @@ export default function page() {
   const table = [
     {
       no: "1",
-      tanggal: "26 November 2022",
+      tanggal: "30 June 2023",
       informasi: {
-        subtitle: "Tidak ada informasi terkini",
-        title: "26 November 2022",
-        description: "Tidak ada informasi terkini",
+        subtitle: "PROMO KELAS SPESIAL MEMBER !",
+        title: "Spesial Member yang udah beli Kelas Mastering Laravel !",
+        description: "Kamu bisa dapetin kelas mastering Laravel cuman Rp. 120.000 aja !! cukup pakai kode Voucher ANNIV120 saat checkout kelas! sampai jumpa di kelas 🙌 ",
       },
     },
     {
       no: "2",
-      tanggal: "26 November 2023",
+      tanggal: "30 June 2023",
       informasi: {
-        subtitle: "Tidak ada informasi terkini",
-        title: "26 November 2022",
-        description: "Tidak ada informasi terkini",
+        subtitle: "PROMO KELAS SPESIAL MEMBER !",
+        title: "Spesial Member yang udah beli kelas Fullstack Developer !",
+        description: "Kamu bisa dapeting Kelas Full Stack Developer cuman Rp. 230.000 aja !! cukup pakai kode Voucher ANNIV230 saat checkout kelas! Di kelas Full Stack Developer ini kamu juga akan mendapatkan Grup konsultasi khusus buat kamu yang udah beli kelas di Onlenkan Tunggu apalagi? sampai jumpa dikelas 🙌 ",
       },
     },
   ];
