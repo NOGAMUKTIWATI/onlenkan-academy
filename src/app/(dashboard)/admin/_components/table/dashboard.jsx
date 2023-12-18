ee645cf2dbd6d44d8757cc97adf41c046463ad2a
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-12-19 00:41:14 +0700

    dashboard admin

---
+++ src/app/(dashboard)/admin/_components/table/dashboard.jsx
@@ -0,0 +1,62 @@
+import { Icon } from "@iconify/react";
+
+export default function dashboard({ thead, td }) {
+  return (
+    <div className="p-1 w-full rounded-md">
+      <div className="grid bg-white p-5 grid-cols-5  w-full text-center">
+        {thead.map((item, index) => (
+          <h2 key={index}>{item}</h2>
+        ))}
+      </div>
+      <div className="w-full mt-5">
+        <div className="w-full space-y-5">
+          {td.map((item, i) => (
+            <div
+              className="bg-white rounded-md grid p-3 gap-5 grid-cols-5 justify-start text-left"
+              key={i}
+            >
+              <div className="w-full space-y-4">
+                <h2 className="text-center mt-7 font-medium text-black">
+                  {item.tanggal}
+                </h2>
+              </div>
+              <div className="w-full space-y-2">
+                <p>{item.informasi.name}</p>
+                <p className="text-primary">{item.informasi.email}</p>
+                <p className="text-primary">{item.informasi.nomor}</p>
+              </div>
+              <div className="w-full flex justify-center text-center mt-8 ">
+                <div>
+                  <p>{item.harga}</p>
+                  <button className="px-5 py-2 text-white bg-detail rounded-md flex gap-2">
+                    <Icon icon="ri:information-line" className="mt-1" />
+                    <p>Cek Payment</p>
+                  </button>
+                </div>
+              </div>
+              <div className="mt-7 text-center">{item.videoTerakhir}</div>
+              <div className=" gap-2">
+                <div className="flex gap-2 mt-7 max-w-[200px]">
+                  <select
+                    name=""
+                    className="w-full px-5 py-2 bg-gray-100"
+                    id=""
+                  >
+                    {item.status.map((item, index) => (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    ))}
+                  </select>
+                  <button className="bg-red-500 p-3 rounded-md text-white">
+                    <Icon icon="mdi:trash-outline" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(dashboard)/admin/page.jsx
+++ src/app/(dashboard)/admin/page.jsx
@@ -1,31 +1,47 @@
 "use client";
 import Card from "./_components/overview/card";
+import TableDashboard from "./_components/table/dashboard";
 
 export default function page() {
   const items = [
     {
       id: 1,
       icon: "material-symbols-light:nest-display-outline",
-      title: "2 Kelas",
-      description: "Kelas yang kamu miliki.",
+      title: "5 E-Course",
+      description: "Kelas yang telah terpublish",
     },
     {
       id: 2,
       icon: "material-symbols-light:frame-source",
-      title: "6 Source",
-      description: "Source code yang dimiliki.",
+      title: "246 Member",
+      description: "Member Kelas",
     },
     {
       id: 3,
       icon: "material-symbols:event",
-      title: "0 Event",
-      description: "Event yang kamu ikuti",
+      title: "9 Resource",
+      description: "Source code yang terpublish",
     },
     {
       id: 4,
       icon: "lets-icons:file-dock-fill",
-      title: "0 Sertifikat",
-      description: "Fitur ini akan segera aktif",
+      title: "6 Artikel",
+      description: "Artikel terpublish pada bulan ini",
+    },
+  ];
+
+  const table = [
+    {
+      tanggal: "7 Agustus 2024",
+      informasi: {
+        name: "Mohammad Sahrullah",
+        email: "mohammadsahrullah894@gmail.com",
+        nomor: "6281336210025",
+      },
+      harga: "Rp. 1.000.000",
+      banner: "../images/kelas/card-1.jpg",
+      videoTerakhir: "background",
+      status: ["Active", "No Active"],
     },
   ];
 
@@ -34,19 +50,40 @@ export default function page() {
       <h2 className="font-bold text-start text-2xl py-5">
         Selamat Datang Kembali, Muhammad Yunus
       </h2>
-      <p className="mt-5 font-bold text-start text-gray-500 text-sm UPPERCASE">
-        OVERVIEW
-      </p>
+      <div className="p-1">
+        <p className="mt-5 font-bold text-start text-gray-500 text-sm uppercase">
+          OVERVIEW
+        </p>
 
-      <div className="overview grid grid-cols-1 lg:grid-cols-4 gap-5 py-2">
-        {items.map((item, index) => (
-          <Card
-            key={index}
-            icon={item.icon}
-            title={item.title}
-            description={item.description}
+        <div className="overview grid grid-cols-1 lg:grid-cols-4 gap-5 py-2">
+          {items.map((item, index) => (
+            <Card
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="p-2">
+        <p className="mt-3 font-bold text-start text-gray-500 text-sm uppercase">
+          MEMBER KELAS TERBARU (1 MEMBER)
+        </p>
+
+        <div className="table-dashboard py-2">
+          <TableDashboard
+            thead={[
+              "Tanggal Bergabung",
+              "Informasi Personal",
+              "Harga Beli",
+              ,
+              "Video Terakhir",
+              "Status",
+            ]}
+            td={table}
           />
-        ))}
+        </div>
       </div>
     </div>
   );
